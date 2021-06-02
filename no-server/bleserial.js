ea932696333738c08b9b0fdf8c2ec5a7e675729d
chillchillchou <hc2446@nyu.edu>
2021-06-02 13:43:58 -0400

    minor change

--- no-server/bleserial.js
+++ no-server/bleserial.js
@@ -108,7 +108,7 @@ function onDisconnected() {
 function handleNotifications(event) {
     console.log('notification');
     let value = event.target.value;
-    // Convert raw data bytes to character values and use these to 
+    // Convert raw data bytes to character values and use these to
     // construct a string.
     let str = "";
     for (let i = 0; i < value.byteLength; i++) {
